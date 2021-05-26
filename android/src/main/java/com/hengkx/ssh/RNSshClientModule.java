ac05d54f1d449b479036334fe1dd2827498c5e10
Tom Xu <ycxzhkx@gmail.com>
2021-05-26 17:41:00 +0800

    fix: android release

--- android/src/main/java/com/hengkx/ssh/RNSshClientModule.java
+++ android/src/main/java/com/hengkx/ssh/RNSshClientModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.hengkx.ssh;
 
 import android.os.Environment;
 import android.util.Log;
--- android/src/main/java/com/hengkx/ssh/RNSshClientPackage.java
+++ android/src/main/java/com/hengkx/ssh/RNSshClientPackage.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.hengkx.ssh;
 
 import java.util.Arrays;
 import java.util.Collections;
